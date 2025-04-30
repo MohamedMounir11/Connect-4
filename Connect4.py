e1c5bdb484074ee81375a5bc7ea326a740a7c949
Mohamed Mounir <mohemmedmunir112006@gmail.com>
2025-04-30 18:27:51 +0300

    Update Connect4.py

--- Connect4.py
+++ Connect4.py
@@ -24,6 +24,41 @@ def get_next_open_row(board, col):
 
 def drop_piece(board, row, col, piece):
 	board[row][col] = piece
+
+def is_terminal_node(board):
+	return winning_move(board, 1) or winning_move(board, 2) or len(get_valid_locations(board)) == 0
+
+def get_valid_locations(board):
+	return [col for col in range(COLUMN_COUNT) if is_valid_location(board, col)]
+
+def print_board(board):
+	print(np.flip(board, 0))
+
+def winning_move(board, piece):
+	# Check horizontal locations for win
+	for c in range(COLUMN_COUNT-3):
+		for r in range(ROW_COUNT):
+			if board[r][c] == piece and board[r][c+1] == piece and board[r][c+2] == piece and board[r][c+3] == piece:
+				return True
+
+	# Check vertical locations for win
+	for c in range(COLUMN_COUNT):
+		for r in range(ROW_COUNT-3):
+			if board[r][c] == piece and board[r+1][c] == piece and board[r+2][c] == piece and board[r+3][c] == piece:
+				return True
+
+	# Check positively sloped diaganols
+	for c in range(COLUMN_COUNT-3):
+		for r in range(ROW_COUNT-3):
+			if board[r][c] == piece and board[r+1][c+1] == piece and board[r+2][c+2] == piece and board[r+3][c+3] == piece:
+				return True
+
+	# Check negatively sloped diaganols
+	for c in range(COLUMN_COUNT-3):
+		for r in range(3, ROW_COUNT):
+			if board[r][c] == piece and board[r-1][c+1] == piece and board[r-2][c+2] == piece and board[r-3][c+3] == piece:
+				return True
+
 def minimax(board, depth, maximizing_player):
 	valid_locations = get_valid_locations(board)
 	terminal = is_terminal_node(board)
